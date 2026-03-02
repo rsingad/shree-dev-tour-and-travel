bea358d0327d51b5830173d98c7c2d1e3ccc1235
rsingad <r.singerjat@gmail.com>
2026-03-02 10:52:22 +0530

    seo performance score improve..

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,19 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
+ theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        brand: {
+          light: "#EAB308",
+          DEFAULT: "#CA8A04",
+          dark: "#A16207",
+        },
+      },
+    },
   },
   plugins: [],
 }
